e8f3bd3780b1d15620bef3b34baa2e1e78eeb5dc
brownie3003 <brownie3003@gmail.com>
2013-07-12 22:22:31 +0100

    Nicks a bummer

--- dashboard/js/charts.js
+++ dashboard/js/charts.js
@@ -9,15 +9,18 @@ var data = {
     {
       "completed_count": 5, 
       "not_completed_count": 2, 
-      "task_label": "functions_with_irb_1"
+      "task_label": "functions_with_irb_2"
     }
   ]
 }
 
-
-
-$(function () {
-    $('#container1').highcharts({
+$(document).ready(function(){
+    for(var key in data.progress) {
+        var obj = data.progress[key];
+        var accordion = '<div class="accordion-group"><div class="accordion-heading"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">' + obj.task_label + '</a></div><div id="collapseTwo" class="accordion-body collapse"><div class="accordion-inner" id="' + obj.task_label + '"></div></div></div>'
+        $(accordion).appendTo('.accordion');
+        $(function () {
+    $('#' + obj.task_label).highcharts({
         chart: {
             plotBackgroundColor: null,
             plotBorderWidth: null,
@@ -45,10 +48,12 @@ $(function () {
             type: 'pie',
             name: 'Browser share',
             data: [
-                ['I don\'t get it',   45.0],
-                ['I get it',       26.8]
+                ['I don\'t get it',   obj.completed_count],
+                ['I get it',       obj.not_completed_count]
             ]
         }]
     });
 });
-
+        console.log(obj.completed_count);
+    }
+});
