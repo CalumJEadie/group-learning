fad012f1fcaea1e41a2ebb92a9103cf8ea3aebc3
Nicholas Angeli <nicangeli@gmail.com>
2013-07-12 15:37:57 +0100

    builds urls

---
+++ js/tracker.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	var urls = []
+	$('div.exercise').each(function(index) {
+		var exercise = this;
+		var h3_before = $(this).prevAll("h3")[0];
+		//console.log($(h3_before).attr('id'));
+		urls.push($(h3_before).attr('id'));
+		//$(exercise).append('<div class="btn-group"><form><button class="btn" formaction="boo.php" type="submit" formmethod="POST">	:)</button><button class="btn">	:|</button><button class="btn">:(</button></form></div>')
+	});
+	var to_submit_to = build_urls(urls);
+	$('div.exercise').each(function(index) {
+		$(this).append('<div class="btn-group"><form><button class="btn" formaction="' + to_submit_to[index] + '" type="submit" formmethod="POST">	:)</button><button class="btn" formaction="' + to_submit_to[index] + '" type="submit" formmethod="POST">	:|</button><button class="btn" formaction="' + to_submit_to[index] + '" type="submit" formmethod="POST">:(</button></form></div>')
+	});
+});
+
+
+
+var build_urls = function(array) {
+	var counts = {};
+	var urls = [];
+	for(var i = 0; i < array.length; i++) {
+		var obj = array[i];
+		counts[obj] = counts[obj] ? counts[obj]+1 : 1;
+	}
+	for(var key in counts) {
+		var val = counts[key];
+		for(var i = 0; i < val; i++) {
+			urls.push(key + '_' + i);
+		}
+	}
+	return urls;
+}
